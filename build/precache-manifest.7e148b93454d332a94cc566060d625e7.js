ff4ce7efd8cf0663ab5922fcffc66d8b3343b2f2
Supritika Bhardwaj <supritikabhardwaj04@gmail.com>
2025-06-25 01:21:00 +0530

    Add files via upload

---
+++ build/precache-manifest.7e148b93454d332a94cc566060d625e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a600907650aec691ebb3182aca7a35c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5eb6f0dc1ba7956499b0",
+    "url": "/static/css/main.1914322d.chunk.css"
+  },
+  {
+    "revision": "30161ec772b3aa35a305",
+    "url": "/static/js/2.2b2bc69c.chunk.js"
+  },
+  {
+    "revision": "f820d615731d9ae43fc2a99e0345bc32",
+    "url": "/static/js/2.2b2bc69c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5eb6f0dc1ba7956499b0",
+    "url": "/static/js/main.68059616.chunk.js"
+  },
+  {
+    "revision": "35f67668c217d61057f5",
+    "url": "/static/js/runtime-main.ed9d8f35.js"
+  }
+]);
